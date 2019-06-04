38d0be087e1d191a76ae9c90553a2040cf90dac5
Ubuntu <ubuntu@ip-172-31-89-97.ec2.internal>
2019-06-04 22:38:37 +0000

    done

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,19 @@
 // Code CoordinatesButton Component Here
+import React from "react"
+
+class CoordinatesButton extends React.Component{
+    
+    createArr=()=>{
+        console.log(this.prop)
+    }
+    
+    
+    render(){
+        
+        return(
+            <button onClick={this.createArr}>
+                create an array
+            </button>
+        )
+    }
+}
